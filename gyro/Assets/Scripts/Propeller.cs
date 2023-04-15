354f837289d726d9993466613f67fd7e1d813f31
Ilya <xumdes@gmail.com>
2023-04-15 10:50:09 +0500

    комит для леши потому что нада

--- gyro/Assets/Scripts/Propeller.cs
+++ gyro/Assets/Scripts/Propeller.cs
@@ -3,26 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Propeller : MonoBehaviour
-{
+{   
     [SerializeField] private float maxPower = 4f;
-    [SerializeField] private float pitchChange = 0.25f;
-    // [SerializeField] private float rollChange = 1.5f;
-    // [SerializeField] private float yawPower = 30f;
 
-    public float GetPropellerVector(Rigidbody rb, InputManager input) {
+    private Rigidbody rb;
+    private InputManager input;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+        input = GetComponentInParent<InputManager>();
+    }
+
+    private void FixedUpdate() {
+        UpdateEngine(rb, input);
+    }
+
+    public void UpdateEngine(Rigidbody rb, InputManager input) {
         float propellerPower = 0;
         propellerPower = ((rb.mass * Physics.gravity.magnitude) + (input.Throttle * maxPower)) / 4;
 
-        if ((name == "BR_Propeller" || name == "BL_Propeller") && input.PitchNRoll.y > 0) {
-            propellerPower += pitchChange;
-        } else if ((name == "FR_Propeller" || name == "FL_Propeller") && input.PitchNRoll.y > 0) {
-            propellerPower -= pitchChange;
-        } else if ((name == "FR_Propeller" || name == "FL_Propeller") && input.PitchNRoll.y < 0) {
-            propellerPower += pitchChange;
-        } else if ((name == "BR_Propeller" || name == "BL_Propeller") && input.PitchNRoll.y < 0) {
-            propellerPower -= pitchChange;
-        }
+        Vector3 forceVector = Vector3.up * propellerPower;
 
-        return propellerPower;
+        rb.AddForce(forceVector, ForceMode.Force);
     }
 }
--- gyro/Assets/Scripts/QuadcopterMovement.cs
+++ gyro/Assets/Scripts/QuadcopterMovement.cs
@@ -6,7 +6,10 @@ public class QuadcopterMovement : MonoBehaviour
 {
     private Rigidbody rb;
     private InputManager input;
-    [SerializeField] private List<GameObject> propellers = new List<GameObject>();
+    [SerializeField] private Propeller FR_propeller;
+    [SerializeField] private Propeller FL_propeller;
+    [SerializeField] private Propeller BR_propeller;
+    [SerializeField] private Propeller BL_propeller;
 
     private void Start() {
         rb = GetComponent<Rigidbody>();
@@ -14,18 +17,6 @@ public class QuadcopterMovement : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        foreach (GameObject propeller in propellers) {
-            Propeller propellerScript = propeller.GetComponent<Propeller>();
 
-            float propellerPower = propellerScript.GetPropellerVector(rb, input);
-            Vector3 propellerVector = new Vector3(transform.rotation.x, propellerPower, transform.rotation.z);
-
-            rb.AddRelativeForce(propellerVector, ForceMode.Force);
-        }
-    }
-
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position + new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z) + rb.centerOfMass, 0.25f);
     }
 }
